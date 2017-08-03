dbe1a3a9b97f51250426ea65f334a4921a90c0e9
Tyler Chermely <tyler846050@aim.com>
2017-08-02 21:22:32 -0400

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Tyler Chermely on 8/2/17.
+//  Copyright © 2017 TylerC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
